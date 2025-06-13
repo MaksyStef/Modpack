6efffcc972fd497c60c5c598bc804493de21f03d
MaksyStef <maksistef@gmail.com>
2025-06-13 12:34:49 +0200

    fill instances, dropdown layout

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 import eel
 import json
 import screeninfo
+import os
 
 @eel.expose
 def get_instances():
-  # try:
-  #   with open('~/AppData/Roaming/.minecraft/launcher_profiles.json', 'r') as instances:
-  #     instances = json.dump(instances.read())
-  #     return instances["profiles"]
-  # except:
-  #   return {}
-  return json.dumps({'One': 2})
+  try:
+    with open(os.path.expanduser('~/AppData/Roaming/.minecraft/launcher_profiles.json'), 'r', encoding="UTF-8") as instances:
+      instances = json.loads(instances.read())["profiles"]
+      for instance_id, instance_props in instances.items():
+        if instance_props.type != "custom":
+          instances.pop(instance_id)
+      print(instances)
+      return instances
+  except Exception as error:
+    return error.__str__()
 
 display = screeninfo.get_monitors()[0]
 
+app_width = display.width//2
+app_height = display.height/1.5
+off_x = (display.width - app_width) // 2
+off_y = (display.height - app_height) // 2
+
 eel.init('web')
-eel.start('index.html', size=(display.width//2, display.height/1.5))
+eel.start('index.html', size=(app_width, app_height), position=(off_x, off_y))
--- web/script.js
+++ web/script.js
@@ -8,4 +8,19 @@ let switchInactive = (e)=> {
 
 exportSwitch.addEventListener('click', switchInactive);
 importSwitch.addEventListener('click', switchInactive);
-console.log(eel.get_instances())
+
+eel.get_instances()()
+  .then((data)=> {
+    let dropdownList = document.querySelector('.dropdown__list');
+    let dropdownActive = document.querySelector('.dropdown__selected');
+    for (const [id, props] of Object.entries(data)) {
+      let item = document.createElement('div');
+      item.className = "dropdown__item";
+      item.setAttribute("data-id", id);
+      if (props.icon) { item.setAttribute("data-img", props.icon) };
+      item.innerText = props.name;
+      dropdownList.appendChild(item);
+    }
+    dropdownActive.innerHTML = dropdownList.firstElementChild.outerHTML;
+    dropdownList.firstElementChild.remove();
+  })

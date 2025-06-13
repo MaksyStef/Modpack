acbcda9e52a0cecce88cd893b34f772d3c291f46
MaksyStef <maksistef@gmail.com>
2025-06-13 07:55:41 +0200

    define basic app workspace

---
+++ web/script.js
@@ -0,0 +1,10 @@
+let exportSwitch = document.querySelector('.tabs__export');
+let importSwitch = document.querySelector('.tabs__import');
+
+let switchInactive = (e)=> {
+  e.target.parentElement.querySelector('.active').classList.remove('active');
+  e.target.classList.add('active');
+}
+
+exportSwitch.addEventListener('click', switchInactive);
+importSwitch.addEventListener('click', switchInactive);

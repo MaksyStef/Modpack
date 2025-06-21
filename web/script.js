55bc2a8f1f52b1d0243e0ba1bbcd867f34da4d96
MaksyStef <maksistef@gmail.com>
2025-06-21 02:15:03 +0200

    optimize selectDropdownItem function

--- web/script.js
+++ web/script.js
@@ -9,18 +9,38 @@ let switchInactive = (e)=> {
 exportSwitch.addEventListener('click', switchInactive);
 importSwitch.addEventListener('click', switchInactive);
 
+const dropdownList = document.querySelector('.dropdown__list');
+const dropdownSelected = document.querySelector('.dropdown__selected');
+
+let createDropdownItem = (id, props) => {
+  // Create element
+  let item = document.createElement('div');
+  item.className = "dropdown__item";
+  item.setAttribute("data-id", id);
+  if (props.icon) { item.setAttribute("data-img", props.icon) };
+  
+  item.innerText = props.name;
+  
+  // Create getProps() method
+  item.getProps = ()=> (id, props);
+  // Set onClick event listener
+  item.addEventListener(
+    'click', ()=> selectDropdownItem(item)
+  )
+
+  // Append list
+  dropdownList.appendChild(item);
+}
+
+let selectDropdownItem = (dropdownItem) => {
+  // Append method moves an item from Parent_A to Parent_B
+  dropdownList.appendChild(dropdownSelected.firstChild)
+  dropdownSelected.appendChild(dropdownItem);
+}
 eel.get_instances()()
   .then((data)=> {
-    let dropdownList = document.querySelector('.dropdown__list');
-    let dropdownActive = document.querySelector('.dropdown__selected');
     for (const [id, props] of Object.entries(data)) {
-      let item = document.createElement('div');
-      item.className = "dropdown__item";
-      item.setAttribute("data-id", id);
-      if (props.icon) { item.setAttribute("data-img", props.icon) };
-      item.innerText = props.name;
-      dropdownList.appendChild(item);
+      createDropdownItem(id, props);
     }
-    dropdownActive.innerHTML = dropdownList.firstElementChild.outerHTML;
-    dropdownList.firstElementChild.remove();
+    selectDropdownItem(dropdownList.firstElementChild);
   })
